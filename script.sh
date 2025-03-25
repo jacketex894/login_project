5f6456e3ecca64b64b51cbd469008280de6aefee
junyanzhao <junyanzhao1996@gmail.com>
2025-03-25 14:29:26 +0800

    feat: 🎸 update script for activate project

--- script.sh
+++ script.sh
@@ -1,5 +1,24 @@
-#nginx
+#!/bin/bash
 ENV_FILE="./.env"
+ENV_SAMPLE_FILE="env_template"
+
+if [ ! -f "$ENV_FILE" ]; then
+  echo ".env file does not exist, generating a new one from envsample..."
+  ENV_SAMPLE_CONTENT=$(cat "$ENV_SAMPLE_FILE")
+
+  BACKEND_PORT=$((RANDOM % 10001 + 10000))
+  FRONTEND_PORT=$((RANDOM % 10001 + 10000))
+  DATABASE_PORT=$((RANDOM % 10001 + 10000))
+  MYSQL_ROOT_PASSWORD=$(tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 16)
+  NEW_ENV_CONTENT=$(echo "$ENV_SAMPLE_CONTENT" | \
+        sed "s/BACKEND_PORT=[^ ]*/BACKEND_PORT=$BACKEND_PORT/" | \
+        sed "s/FRONTEND_PORT=[^ ]*/FRONTEND_PORT=$FRONTEND_PORT/" | \
+        sed "s/DATABASE_PORT=[^ ]*/DATABASE_PORT=$DATABASE_PORT/" | \
+        sed "s/MYSQL_ROOT_PASSWORD=[^ ]*/MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD/")
+  echo "$NEW_ENV_CONTENT" > "$ENV_FILE"
+fi
+
+#nginx
 NGINX_TEMPLATE_FILE="./nginx/nginx_template.conf"
 NGINX_CONFIG_FILE="./nginx/nginx.conf"
 
@@ -18,3 +37,15 @@ echo "New Nginx config file generated"
 echo "FRONTEND_PORT : $FRONTEND_PORT"
 echo "BACKEND_PORT : $BACKEND_PORT"
 
+#frontend
+current_dir=$(pwd)
+cd frontend || exit
+npm run build
+cd "$current_dir"
+
+docker compose up -d
+
+echo "Frontend run at http://localhost:$FRONTEND_PORT"
+echo "Backend run at http://localhost:$BACKEND_PORT"
+echo "Database run at http://localhost:$BACKEND_PORT"
+echo "Check api document at http://localhost:$BACKEND_PORT/redoc"
