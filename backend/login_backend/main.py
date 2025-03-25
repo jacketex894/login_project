ec80805d61047337732572f3aa94bdd9d1ee7f13
junyanzhao <junyanzhao1996@gmail.com>
2025-03-25 21:01:45 +0800

    feat: 🎸 add Hash

--- backend/login_backend/main.py
+++ backend/login_backend/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from typing import TypedDict
 from fastapi.middleware.cors import CORSMiddleware
 
-from system.lib.hash import hash_password
 
 app = FastAPI()
 
@@ -17,6 +16,4 @@ class LoginRequest(TypedDict):
 
 @app.post("/api/login")
 async def login(loginrequest:LoginRequest):
-    print(loginrequest)
-    print(f"Hashed Password: {hash_password(loginrequest['password'])}")
     return {"message":"login"}
