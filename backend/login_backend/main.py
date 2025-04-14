66b71ace0b4fad9b2e2df2843a69d93ff8f78090
junyanzhao <junyanzhao1996@gmail.com>
2025-04-14 17:37:12 +0800

    feat: 🎸 add login api

--- backend/login_backend/main.py
+++ backend/login_backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI,Request
 from typing import TypedDict
 from typing import Union
 
-from .util.UserProfile import UserProfileFactory,RegisterRequest
+from .util.UserProfile import UserProfileFactory,RegisterRequest,LoginRequest
 app = FastAPI()
 
 class ErrorResponse(TypedDict):
@@ -13,13 +13,14 @@ class ErrorResponse(TypedDict):
                                                                                    "description":"User name or mail exist error"}})
 def user_register(register_request:RegisterRequest, request: Request):
     client_ip = request.client.host 
-    return UserProfileFactory.register('Normal',register_request,client_ip)
+    user_profile_handler = UserProfileFactory.get_profile_handler('UsernamePassword')
+    return user_profile_handler.register(register_request,client_ip)
 
 
-class LoginRequest(TypedDict):
-    account:str
-    password:str
 
-@app.post("/api/login",tags=["users"])
-async def login(loginrequest:LoginRequest):
-    return {"message":"login"}
+@app.post("/api/login",tags=["users"], response_model=Union[dict, ErrorResponse], responses={400: {"model": ErrorResponse,
+                                                                                   "description":"User name or password wrong"}})
+async def login(login_request:LoginRequest, request: Request):
+    client_ip = request.client.host
+    user_profile_handler = UserProfileFactory.get_profile_handler('UsernamePassword')
+    return user_profile_handler.login(login_request,client_ip)
