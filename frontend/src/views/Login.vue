45fbc8218b0bfce11f4dcb6343218d85a5f266ef
junyanzhao <junyanzhao1996@gmail.com>
2025-06-13 15:21:50 +0800

    fix: 🐛 login fail but stil redirect page

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -59,14 +59,14 @@ export default {
                 let response = await login(userData);
                 this.showAlert = "true"
                 this.alertMessage = "Login sucess"
-                this.alertType = 'success'        
+                this.alertType = 'success'    
+                this.$router.push("/spending-analysis")    
             }
             catch (error){
                 this.showAlert = "true"
                 this.alertMessage = "Login fail : The user name or password  is wrong, please check."
                 this.alertType = 'error'
             }
-            this.$router.push("/spending-analysis")
         }
     }
 }
